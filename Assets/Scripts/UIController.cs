4d43911e705f386eaa352f62a848dfb9b67249f6
Phạm Thị Thanh Trúc <chuckieahihi@gmail.com>
2025-09-16 18:07:47 +0700

    fix bug
    fix bug

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,43 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class UIController : MonoBehaviour
 {
-    public static UIController Instance;
+    // KHÔNG CÓ DÒNG "public static UIController Instance;" thừa ở đây nữa
+
     [SerializeField] private Slider playerHealthSlider;
     [SerializeField] private TMP_Text healthText;
     [SerializeField] private Slider playerExperienceSlider;
     [SerializeField] private TMP_Text experienceText;
     public GameObject gameOverPanel;
     public GameObject pausePanel;
+    public GameObject levelUpPanel;
     [SerializeField] private TMP_Text timerText;
 
-    void Awake(){
-    if ( Instance != null && Instance != this){
-        Destroy(this);
-    } else {
-        Instance = this;
+    public LevelUpButton[] levelUpButtons;
+
+    // ĐÂY LÀ ĐOẠN CODE SINGLETON ĐÃ ĐƯỢC SỬA LẠI CHO ĐÚNG CÚ PHÁP
+    private static UIController _instance;
+    public static UIController Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<UIController>();
+            }
+            return _instance;
+        }
     }
-   }
 
-   public void UpdateHeathSlider(){
-    playerHealthSlider.maxValue = PlayerController.Instance.playerMaxHealth;
-    playerHealthSlider.value = PlayerController.Instance.playerHealth;
-    healthText.text = playerHealthSlider.value + "/" + playerHealthSlider.maxValue;
-   }
+    void Awake(){
+        if (_instance != null && _instance != this){
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
 
-   public void UpdateExperienceSlider(){
-    playerExperienceSlider.maxValue = PlayerController.Instance.playerLevels[PlayerController.Instance.currentLevel -1];
-    playerExperienceSlider.value = PlayerController.Instance.experience;
-    experienceText.text = playerExperienceSlider.value + "/" + playerExperienceSlider.maxValue;
-   }
+    public void UpdateHealthSlider(){
+        playerHealthSlider.maxValue = PlayerController.Instance.playerMaxHealth;
+        playerHealthSlider.value = PlayerController.Instance.playerHealth;
+        healthText.text = playerHealthSlider.value + " / " + playerHealthSlider.maxValue;
+    }
 
-   public void UpdateTimer(float timer){
-    float min = Mathf.Floor(timer / 60f);
-    float sec = Mathf.Floor(timer % 60f);
-    timerText.text = min + ":" + sec.ToString("00");
-   }
+    public void UpdateExperienceSlider(){
+        playerExperienceSlider.maxValue = PlayerController.Instance.playerLevels[PlayerController.Instance.currentLevel - 1];
+        playerExperienceSlider.value = PlayerController.Instance.experience;
+        experienceText.text = playerExperienceSlider.value + " / " + playerExperienceSlider.maxValue;
+    }
+
+    public void UpdateTimer(float timer){
+        float min = Mathf.FloorToInt(timer / 60f);
+        float sec = Mathf.FloorToInt(timer % 60f);
+
+        timerText.text = min + ":" + sec.ToString("00");
+    }
+
+    public void LevelUpPanelOpen(){
+        levelUpPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void LevelUpPanelClose(){
+        levelUpPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
 }
--- Assets/Scripts/Utils/DamageNumberController.cs
+++ Assets/Scripts/Utils/DamageNumberController.cs
@@ -7,16 +7,16 @@ public class DamageNumberController : MonoBehaviour
     public static DamageNumberController Instance;
     public DamageNumber prefab;
 
-    private void Awake() {
-        if ( Instance != null && Instance != this){
+    private void Awake(){
+        if (Instance != null && Instance != this){
             Destroy(this);
         } else {
             Instance = this;
         }
     }
 
-    public void CreateNumber (float value, Vector3 location){
-       DamageNumber damageNumber = Instantiate (prefab, location, transform.rotation, transform);
-       damageNumber.SetValue (Mathf.RoundToInt(value));
+    public void CreateNumber(float value, Vector3 location){
+        DamageNumber damageNumber = Instantiate(prefab, location, transform.rotation, transform);
+        damageNumber.SetValue(Mathf.RoundToInt(value));
     }
 }
