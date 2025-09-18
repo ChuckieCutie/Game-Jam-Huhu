7e91ea9e2f22f0fac36cf4946e82ce98239f71ff
Phạm Thị Thanh Trúc <chuckieahihi@gmail.com>
2025-09-18 08:14:37 +0700

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,55 +6,55 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
 
-    public AreaWeapon areaWeapon; 
-    public DirectionalWeapon directionalWeapon; 
-    public SpinWeapon spinWeapon; 
-    public SlowTrapWeapon slowTrapWeapon; 
-    public BounceBallWeapon bounceBallWeapon;
+    // --- CẤU TRÚC VŨ KHÍ ---
+    [Header("Weapon Slots")]
+    public DirectionalWeapon directionalWeapon; // Vũ khí Q (cố định)
 
-    private float areaWeaponCooldown;
-    private float directionalWeaponCooldown;
-    private float spinWeaponCooldown;
-    private float slowTrapWeaponCooldown;
-    private float bounceBallWeaponCooldown;
+    private Weapon weaponSlotW;
+    private Weapon weaponSlotE;
+    private Weapon weaponSlotR;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Animator animator;
+    private float cooldownQ, cooldownW, cooldownE, cooldownR;
 
+    [Header("Weapon Management")]
+    [SerializeField] private List<Weapon> inactiveWeaponsPool; // Pool vũ khí chưa mở khóa
+    public List<Weapon> activeWeapons;
+    [SerializeField] private List<Weapon> upgradeableWeapons;
+    public List<Weapon> maxLevelWeapons;
+
+    [Header("Player Stats")]
     [SerializeField] private float moveSpeed;
     public Vector3 playerMoveDirection;
     public Vector3 lastMoveDirection;
     public float playerMaxHealth;
     public float playerHealth;
 
+    [Header("Experience & Level")]
     public int experience;
     public int currentLevel;
     public int maxLevel;
+    public List<int> playerLevels;
 
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    
     private Vector3 targetPosition;
-
-    [SerializeField] private List<Weapon> inactiveWeapons;
-    public List<Weapon> activeWeapons;
-    [SerializeField] private List<Weapon> upgradeableWeapons;
-    public List<Weapon> maxLevelWeapons;
-
     private bool isImmune;
     [SerializeField] private float immunityDuration;
     [SerializeField] private float immunityTimer;
 
-    public List<int> playerLevels;
-    
-    void Awake(){
-        if (Instance != null && Instance != this){
-            Destroy(this);
-        } else {
-            Instance = this;
-        }
+    void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); } 
+        else { Instance = this; }
     }
 
-    void Start(){
+    void Start()
+    {
         lastMoveDirection = new Vector3(0, -1);
-        for (int i = playerLevels.Count; i < maxLevel; i++){
+        for (int i = playerLevels.Count; i < maxLevel; i++)
+        {
             playerLevels.Add(Mathf.CeilToInt(playerLevels[playerLevels.Count - 1] * 1.1f + 15));
         }
         playerHealth = playerMaxHealth;
@@ -62,27 +62,32 @@ public class PlayerController : MonoBehaviour
         UIController.Instance.UpdateExperienceSlider();
         targetPosition = transform.position;
 
-        if (directionalWeapon != null && !activeWeapons.Contains(directionalWeapon)){
-            activeWeapons.Add(directionalWeapon);
-            directionalWeapon.gameObject.SetActive(true);
-
-        if (inactiveWeapons.Contains(directionalWeapon)){
-            inactiveWeapons.Remove(directionalWeapon);}
+        // Bắt đầu với vũ khí Q
+        activeWeapons.Add(directionalWeapon);
+        directionalWeapon.gameObject.SetActive(true);
     }
 
     void Update()
     {
+        HandleMovement();
+        HandleImmunity();
+        HandleCooldowns();
+        HandleWeaponInputs();
+    }
+
+    void HandleMovement()
+    {
+        // Trả lại di chuyển bằng chuột phải
         if (Input.GetMouseButtonDown(1)) 
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = transform.position.z; 
+            targetPosition.z = transform.position.z;
         }
 
         if (Vector3.Distance(transform.position, targetPosition) > 0.1f && Time.timeScale != 0)
         {
             playerMoveDirection = (targetPosition - transform.position).normalized;
             lastMoveDirection = playerMoveDirection;
-            
             animator.SetBool("moving", true);
             animator.SetFloat("moveX", playerMoveDirection.x);
             animator.SetFloat("moveY", playerMoveDirection.y);
@@ -92,52 +97,124 @@ public class PlayerController : MonoBehaviour
             playerMoveDirection = Vector3.zero;
             animator.SetBool("moving", false);
         }
-        
-        if (immunityTimer > 0){
-            immunityTimer -= Time.deltaTime;
-        } else {
-            isImmune = false;
-        }
+    }
+    
+    void HandleCooldowns()
+    {
+        if (cooldownQ > 0) cooldownQ -= Time.deltaTime;
+        if (cooldownW > 0) cooldownW -= Time.deltaTime;
+        if (cooldownE > 0) cooldownE -= Time.deltaTime;
+        if (cooldownR > 0) cooldownR -= Time.deltaTime;
+    }
 
-        if (areaWeaponCooldown > 0) areaWeaponCooldown -= Time.deltaTime;
-        if (directionalWeaponCooldown > 0) directionalWeaponCooldown -= Time.deltaTime;
-        if (spinWeaponCooldown > 0) spinWeaponCooldown -= Time.deltaTime;
-        if (slowTrapWeaponCooldown > 0) slowTrapWeaponCooldown -= Time.deltaTime;
-        if (bounceBallWeaponCooldown > 0) bounceBallWeaponCooldown -= Time.deltaTime;
-
-        // Phím w cho Area Weapon
-        if (Input.GetKeyDown(KeyCode.W) && areaWeapon != null && areaWeaponCooldown <= 0)
-        {
-            areaWeapon.ActivateWeapon();
-            areaWeaponCooldown = areaWeapon.stats[areaWeapon.weaponLevel].cooldown; // Đặt lại thời gian hồi chiêu
-        }
-
-        // Phím W cho Directional Weapon
-        if (Input.GetKeyDown(KeyCode.Q) && directionalWeapon != null && directionalWeaponCooldown <= 0)
+    void HandleWeaponInputs()
+    {
+        // Phím Q (cố định)
+        if (Input.GetKeyDown(KeyCode.Q) && directionalWeapon != null && cooldownQ <= 0)
         {
             directionalWeapon.ActivateWeapon();
-            directionalWeaponCooldown = directionalWeapon.stats[directionalWeapon.weaponLevel].cooldown;
+            cooldownQ = directionalWeapon.stats[directionalWeapon.weaponLevel].cooldown;
         }
 
-        // Phím E cho Spin Weapon
-        if (Input.GetKeyDown(KeyCode.E) && spinWeapon != null && spinWeaponCooldown <= 0)
+        // Phím W
+        if (Input.GetKeyDown(KeyCode.W) && weaponSlotW != null && cooldownW <= 0)
         {
-            spinWeapon.ActivateWeapon();
-            spinWeaponCooldown = spinWeapon.stats[spinWeapon.weaponLevel].cooldown;
+            ActivateWeaponInSlot(weaponSlotW, ref cooldownW);
         }
 
-        if (Input.GetMouseButtonDown(1) && slowTrapWeapon != null && slowTrapWeaponCooldown <= 0)
+        // Phím E
+        if (Input.GetKeyDown(KeyCode.E) && weaponSlotE != null && cooldownE <= 0)
+        {
+            ActivateWeaponInSlot(weaponSlotE, ref cooldownE);
+        }
+
+        // Phím R
+        if (Input.GetKeyDown(KeyCode.R) && weaponSlotR != null && cooldownR <= 0)
+        {
+            ActivateWeaponInSlot(weaponSlotR, ref cooldownR);
+        }
+    }
+    
+    // Hàm phụ trợ để kích hoạt vũ khí trong ô, xử lý các trường hợp đặc biệt
+    void ActivateWeaponInSlot(Weapon weapon, ref float cooldown)
+    {
+        if (weapon is SlowTrapWeapon slowTrap)
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0; 
-
-            slowTrapWeapon.ActivateWeapon(mousePosition);
-            
-            slowTrapWeaponCooldown = slowTrapWeapon.stats[slowTrapWeapon.weaponLevel].cooldown;
+            mousePosition.z = 0;
+            slowTrap.ActivateWeapon(mousePosition);
         }
-    }
+        else // Dành cho các vũ khí khác không cần vị trí chuột (Area, Spin, BounceBall)
+        {
+            weapon.SendMessage("ActivateWeapon", SendMessageOptions.DontRequireReceiver);
+        }
+        
+        cooldown = weapon.stats[weapon.weaponLevel].cooldown;
     }
 
+    void HandleImmunity()
+    {
+        if (immunityTimer > 0) { immunityTimer -= Time.deltaTime; }
+        else { isImmune = false; }
+    }
+
+    public void LevelUp()
+    {
+        experience -= playerLevels[currentLevel - 1];
+        currentLevel++;
+        UIController.Instance.UpdateExperienceSlider();
+
+        upgradeableWeapons.Clear();
+        
+        // Kiểm tra xem còn ô W, E, hoặc R trống không
+        if (weaponSlotW == null || weaponSlotE == null || weaponSlotR == null)
+        {
+            // Nếu còn, hiển thị các vũ khí mới có thể chọn
+            upgradeableWeapons.AddRange(inactiveWeaponsPool);
+        }
+        else // Nếu tất cả các ô đã có vũ khí
+        {
+            // Hiển thị các vũ khí đang sở hữu để nâng cấp
+            upgradeableWeapons.AddRange(activeWeapons.Where(w => w.weaponLevel < w.stats.Count - 1));
+        }
+        
+        for (int i = 0; i < UIController.Instance.levelUpButtons.Length; i++)
+        {
+            if (i < upgradeableWeapons.Count)
+            {
+                UIController.Instance.levelUpButtons[i].ActivateButton(upgradeableWeapons[i]);
+                UIController.Instance.levelUpButtons[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                UIController.Instance.levelUpButtons[i].gameObject.SetActive(false);
+            }
+        }
+
+        UIController.Instance.LevelUpPanelOpen();
+    }
+    
+    public void AssignWeaponToSlot(Weapon newWeapon)
+    {
+        // Gán vũ khí mới vào ô trống đầu tiên tìm thấy
+        if (weaponSlotW == null) 
+        {
+            weaponSlotW = newWeapon;
+        }
+        else if (weaponSlotE == null) 
+        {
+            weaponSlotE = newWeapon;
+        }
+        else if (weaponSlotR == null) 
+        {
+            weaponSlotR = newWeapon;
+        }
+        
+        newWeapon.gameObject.SetActive(true);
+        activeWeapons.Add(newWeapon);
+        inactiveWeaponsPool.Remove(newWeapon);
+    }
+    
     void FixedUpdate(){
         rb.velocity = new Vector3(playerMoveDirection.x * moveSpeed, playerMoveDirection.y * moveSpeed);
     }
@@ -158,60 +235,21 @@ public class PlayerController : MonoBehaviour
     public void GetExperience(int experienceToGet){
         experience += experienceToGet;
         UIController.Instance.UpdateExperienceSlider();
-        if (experience >= playerLevels[currentLevel - 1]){
+        if (currentLevel < maxLevel && experience >= playerLevels[currentLevel - 1]){
             LevelUp();
         }
     }
-
-    public void LevelUp(){
-        experience -= playerLevels[currentLevel - 1];
-        currentLevel++;
-        UIController.Instance.UpdateExperienceSlider();
-
-        upgradeableWeapons.Clear();
-
-        if (activeWeapons.Count > 0){
-            upgradeableWeapons.AddRange(activeWeapons);
-        }
-        if (inactiveWeapons.Count > 0){
-            upgradeableWeapons.AddRange(inactiveWeapons);
-        }
-        for (int i = 0; i < UIController.Instance.levelUpButtons.Length; i++){
-            if (upgradeableWeapons.ElementAtOrDefault(i) != null){
-                UIController.Instance.levelUpButtons[i].ActivateButton(upgradeableWeapons[i]);
-                UIController.Instance.levelUpButtons[i].gameObject.SetActive(true);
-            } else {
-                UIController.Instance.levelUpButtons[i].gameObject.SetActive(false);
-            }
-        }
-
-        UIController.Instance.LevelUpPanelOpen();
-    }
-
-    private void AddWeapon(int index){
-        activeWeapons.Add(inactiveWeapons[index]);
-        inactiveWeapons[index].gameObject.SetActive(true);
-        inactiveWeapons.RemoveAt(index);
-    }
-
-    public void ActivateWeapon(Weapon weapon){
-        weapon.gameObject.SetActive(true);
-        activeWeapons.Add(weapon);
-        inactiveWeapons.Remove(weapon);
-    }
-
+    
     public void IncreaseMaxHealth(int value){
         playerMaxHealth += value;
         playerHealth = playerMaxHealth;
         UIController.Instance.UpdateHealthSlider();
-
         UIController.Instance.LevelUpPanelClose();
         AudioController.Instance.PlaySound(AudioController.Instance.selectUpgrade);
     }
 
     public void IncreaseMovementSpeed(float multiplier){
         moveSpeed *= multiplier;
-
         UIController.Instance.LevelUpPanelClose();
         AudioController.Instance.PlaySound(AudioController.Instance.selectUpgrade);
     }
--- Assets/Scripts/Utils/LevelUpButton.cs
+++ Assets/Scripts/Utils/LevelUpButton.cs
@@ -1,34 +1,51 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelUpButton : MonoBehaviour
 {
-    public TMP_Text weaponName;
-    public TMP_Text weaponDescription;
-    public Image weaponIcon;
+    [SerializeField] private Image weaponIcon;
+    [SerializeField] private TMP_Text weaponNameText;
+    [SerializeField] private TMP_Text weaponDescriptionText;
 
     private Weapon assignedWeapon;
 
-    public void ActivateButton(Weapon weapon){
-        if (weapon.gameObject.activeSelf == true){
-            weaponName.text = weapon.name;
-            weaponDescription.text = weapon.stats[weapon.weaponLevel].description;
-        } else {
-            weaponName.text = "NEW " + weapon.name;
-            weaponDescription.text = weapon.basicDescription;
-        }
-
-        weaponIcon.sprite = weapon.weaponImage;
-
+    public void ActivateButton(Weapon weapon)
+    {
         assignedWeapon = weapon;
+        
+        weaponIcon.sprite = weapon.weaponImage;
+        weaponNameText.text = weapon.name;
+
+        int nextLevel = weapon.weaponLevel + 1;
+        if (PlayerController.Instance.activeWeapons.Contains(weapon))
+        {
+            if (nextLevel < weapon.stats.Count)
+            {
+                weaponDescriptionText.text = "LVL " + (nextLevel + 1) + "\n" + weapon.stats[nextLevel].description;
+            }
+            else
+            {
+                weaponDescriptionText.text = "MAX LEVEL";
+            }
+        }
+        else
+        {
+            weaponDescriptionText.text = "NEW\n" + weapon.stats[0].description;
+        }
     }
 
-    public void SelectUpgrade(){
-        if (assignedWeapon.gameObject.activeSelf == true){
+    public void SelectUpgrade()
+    {
+        if (assignedWeapon == null) return;
+
+        if (PlayerController.Instance.activeWeapons.Contains(assignedWeapon))
+        {
             assignedWeapon.LevelUp();
-        } else {
-            PlayerController.Instance.ActivateWeapon(assignedWeapon);
+        }
+        else
+        {
+            PlayerController.Instance.AssignWeaponToSlot(assignedWeapon);
         }
 
         UIController.Instance.LevelUpPanelClose();
